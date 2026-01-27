5f3ed0bb049e7cc67c956f71d9a6da990f020f98
Prachi Kushwaha <prachiku673@gmail.com>
2026-01-27 06:10:10 +0530

    update and type1,2 query added

--- segement_tree/build.cpp
+++ segement_tree/build.cpp
@@ -14,6 +14,34 @@ void build(int ind, int low, int high, int arr[], int seg[]){
 
 }
 
+int query(int ind, int low, int high, int l, int r, int seg[]){
+    // no overlap
+    if(l > high || r < low){
+        return INT_MAX;
+    }
+
+    // complete overlap
+    if(low >= l && high <= r) return seg[ind];
+
+    // partial overlap
+    int mid = (low+high) >>1;
+    int left = query(2*ind+1, low, mid, l, r, seg);
+    int right = query(2*ind+2, mid+1, high, l, r, seg);
+    return min(left,right);
+}
+
+void update(int ind, int low, int high, int i, int val, int seg[]){
+    if(low==high){
+        seg[ind] = val;
+        return;
+    }
+
+    int mid = (low+high) >> 1;
+    if(i<=mid) update(2*ind+1, low, mid, i, val, seg);
+    else update(2*ind+2, mid+1, high, i, val, seg);
+    seg[ind] = min(seg[2*ind+1], seg[2*ind+2]);
+}
+
 void solve(){
     int n;
     cin>>n;
@@ -21,9 +49,24 @@ void solve(){
     for(int i=0; i<n; i++) cin>>arr[i];
     int seg[4*n];
     build(0, 0, n-1, arr, seg);
-    for(int i = 0; i < 2*n; i++)
-    cout << seg[i] << " ";
-
+    // for(int i = 0; i < 2*n; i++)
+    // cout << seg[0] <<endl;
+    int q;
+    cin>>q;
+    while(q--){
+        int type;
+        cin>>type;
+        if(type==1){
+            int l,r;
+            cin>>l>>r;
+            cout << query(0,0,n-1,l,r,seg) << endl;
+        }else{
+            int i,val;
+            cin>>i>>val;
+            update(0,0,n-1,i,val,seg);
+            arr[i] = val;
+        }
+    }
 }
 
 signed main(){
