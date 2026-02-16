0db151141f559a4ed471c026d8cfa4a1dd4309b1
Prachi Kushwaha <prachiku673@gmail.com>
2026-02-16 23:40:20 +0530

    cherrypickup memoization code

--- dp/cherryPickup.cpp
+++ dp/cherryPickup.cpp
@@ -1,5 +1,61 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+class Solution {
+    int f(int index, int rb1, int rb2,
+          vector<vector<int>>& grid,
+          vector<vector<vector<int>>>& dp){
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        if(rb1 < 0 || rb1 >= m || rb2 < 0 || rb2 >= m)
+            return -1e8;
+
+        if(index == n-1){
+            if(rb1 == rb2)
+                return grid[index][rb1];
+            else
+                return grid[index][rb1] + grid[index][rb2];
+        }
+
+        if(dp[index][rb1][rb2] != -1)
+            return dp[index][rb1][rb2];
+
+        int maxi = -1e8;
+
+        for(int j1 = -1; j1 <= 1; j1++){
+            for(int j2 = -1; j2 <= 1; j2++){
+
+                int value = 0;
+
+                if(rb1 == rb2)
+                    value = grid[index][rb1];
+                else
+                    value = grid[index][rb1] + grid[index][rb2];
+
+                value += f(index+1, rb1+j1, rb2+j2, grid, dp);
+
+                maxi = max(maxi, value);
+            }
+        }
+
+        return dp[index][rb1][rb2] = maxi;
+    }
+
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(m, vector<int>(m, -1))
+        );
+
+        return f(0, 0, m-1, grid, dp);
+    }
+};
 
 int main() {
 
